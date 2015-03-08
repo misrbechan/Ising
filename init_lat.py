e61592380e65d46830160582f08a19d0501e7a17
misrbechan <misrbechan@gmail.com>
2015-03-08 18:47:05 +0100

    forgot beta

--- init_lat.py
+++ init_lat.py
@@ -1,12 +1,17 @@
 # preliminary initial lattice
 import numpy as np
 n = 2  # dimension lattice
-R = np.zeros((n,n)) + np.random.choice([-1,1],p=[0.5, 0.5]) #total system up/down with p=0.5
 J = 1
 H = 1
 E_J = 0
+T = 1
+kb = 1.38 *10**-23
+beta = (kb*T)**-1
+
+R = np.zeros((n,n)) + np.random.choice([-1,1],p=[0.5, 0.5]) #total system up/down with p=0.5
 for i in range (n):
     E_H = -H * np.sum(R)
     for j in range (n):
         E_J += -J * np.sum(R[i,j] * R)
-E = E_J + E_H 
+
+E = beta * (E_J + E_H )
